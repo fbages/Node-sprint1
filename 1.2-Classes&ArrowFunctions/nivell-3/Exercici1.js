983e2a547969ba30ad3751b265aad02116ca716f
deltakosmos <francescbages@gmail.com>
2022-05-16 17:57:47 +0200

    1.2 - nivell 3 - Exercici1

---
+++ 1.2-Classes&ArrowFunctions/nivell-3/Exercici1.js
@@ -0,0 +1,10 @@
+//Funcio creadora
+function Cotxes(marca){
+    this.model = marca;
+}
+
+let cotxeJoan = new Cotxes("Hyundai");
+let cotxeMarta = new Cotxes("Seat");
+let cotxeFrancesc = new Cotxes("VW");
+
+console.log(cotxeMarta);
