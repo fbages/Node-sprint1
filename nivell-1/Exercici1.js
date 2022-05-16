75ab10fda6f901fcfeca09e38ea195e0c516bb36
unknown <francescbages@gmail.com>
2022-05-16 14:55:14 +0200

    nivell 1 Exercic1

---
+++ nivell-1/Exercici1.js
@@ -0,0 +1,18 @@
+function imprimirnom(nom, cognom1, cognom2){
+  console.log("El teu nom és : " + nom, cognom1,cognom2);
+}
+
+imprimirnom("Francesc","Bages", "Sabarich")
+
+/*
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Com et dius?', (name) => {
+    console.log(`Hola ${name}! Benvingut`);
+    readline.close();
+  });
+
+  */
