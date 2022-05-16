de5c9f0881482c6e71e477adc4cf443e54789542
unknown <francescbages@gmail.com>
2022-05-16 15:01:02 +0200

    nivell 2 Exercici1

---
+++ nivell-2/Exercici1.js
@@ -0,0 +1,10 @@
+const NOM = `Francesc`;
+const COGNOM1 = `Bages`;
+const COGNOM2 = `Sabarich`;
+
+function imprimirnom(nom, cognom1, cognom2){
+    console.log(`El teu nom és : ${nom} ${cognom1} ${cognom2}`);
+  }
+  
+  imprimirnom(NOM, COGNOM1, COGNOM2);
+  
