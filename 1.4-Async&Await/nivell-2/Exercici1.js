4114c0d3371c332905dfa9c24442963293f0acba
unknown <francescbages@gmail.com>
2022-05-17 12:51:44 +0200

    1.4 - nivell 2 - Exercici1

--- 1.4-Async&Await/nivell-2/Exercici1.js
+++ 1.4-Async&Await/nivell-2/Exercici1.js
@@ -9,6 +9,20 @@ function funcCalcul(valor){
 async function funcDoble(valor){
     const resultat = await funcCalcul(valor);
     console.log(`El doble de ${valor} és ${resultat}`)
+    return resultat;
 }
 
-funcDoble(100);
+//funcDoble(100);
+
+async function dobles(valor1, valor2, valor3){
+    let suma = 0;
+    suma += await funcDoble(valor1);
+    console.log(suma);
+    suma += await funcDoble(valor2);
+    console.log(suma);
+    suma += await funcDoble(valor3);
+    console.log(suma);
+    return console.log(`La suma total és : ${suma}`);
+}
+
+dobles(20,13,4);
