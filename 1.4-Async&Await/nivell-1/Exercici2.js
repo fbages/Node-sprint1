823d26abb5641b726897bd5d10e81be9e0e088b3
deltakosmos <francescbages@gmail.com>
2022-05-17 20:41:10 +0200

    1.4 - nivell 3 - Exercici1 i 2

--- 1.4-Async&Await/nivell-1/Exercici2.js
+++ 1.4-Async&Await/nivell-1/Exercici2.js
@@ -1,19 +1,32 @@
 async function funcAsync(segons) {
-  const resultatPromise = await funcPromise(segons);
-  //No s'executa fins que el await superior s'ha resolt
-  console.log(
-    `Despres d'esperar ${segons} milisegons, s'ha executat ${resultatPromise}`
-  );
+  try{
+    const resultatPromise = await funcPromise(segons);
+    //No s'executa fins que el await superior s'ha resolt
+    console.log(
+      `Despres d'esperar ${segons} milisegons, s'ha executat ${resultatPromise}`
+      );
+    }catch (err){
+        console.log(`No s'ha pogut fer el càlcul`)
+    }
 }
 
 function funcPromise(segons) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("La Promesa");
-    }, segons);
-  });
+    try{
+      if(segons < 0){
+        reject(console.log(`No s'ha pogut executar per falta de temps`));
+      } else if(typeof segons !== 'number'){
+        reject(console.log(`El valor introduït no és un número`));
+      }
+      setTimeout(() => {
+        resolve("La Promesa");
+      }, segons);
+    }catch (err){
+      console.log(err);
+    }
+    });
 }
 
 //Segons a esperar que s'executi la Promise
-const segons = 2;
+const segons = 3 ;
 funcAsync(segons * 1000);
