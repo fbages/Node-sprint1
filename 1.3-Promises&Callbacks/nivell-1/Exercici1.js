9aaff8e08b1bcc9d27f2867dcfe277bce7f945f2
unknown <francescbages@gmail.com>
2022-05-17 09:25:56 +0200

    1.3 - Nivell 1 - Exercici1

--- 1.3-Promises&Callbacks/nivell-1/Exercici1.js
+++ 1.3-Promises&Callbacks/nivell-1/Exercici1.js
@@ -1,8 +1,18 @@
-let promesa1 = new Promise((resolve,reject)=>{
-    if(!true){
-        resolve(console.log("hola"))
-    } else {
-        reject(console.log("adeu"))
-    }
-})
+const input = true;
+const promesa1 = new Promise((resolve, reject) => {
+  if (input) {
+    resolve(console.log("hola"));
+  } else {
+    reject(console.log("adeu"));
+  }
+});
 
+promesa1
+  .then((res) => {
+    //resposta quan la promesa ja està completada
+    console.log(`El programa t'ha saludat amb una promesa`)
+})
+.catch((err) => {
+    //respesta error perquè no s'ha pogut fer la promesa
+    console.log(`El programa no ha resolt la promesa i t'ha rejectat`)
+  });
