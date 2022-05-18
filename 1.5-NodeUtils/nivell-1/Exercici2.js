46e13d4dcbeaccb5d3e5b2a58085c5aceb9650d6
unknown <francescbages@gmail.com>
2022-05-18 10:27:16 +0200

    1.5 - nivell 1 - Exercici2

---
+++ 1.5-NodeUtils/nivell-1/Exercici2.js
@@ -0,0 +1,15 @@
+//Modules
+const fs = require("fs"); //Carrega tot el module amb import fs from 'fs'; podria fer tree-shaking
+const { join } = require("path");
+const path = require("path");
+
+//Constants
+const NOMFITXER = "text.txt";
+const RUTACARPETA = path.join(__dirname, "fitxers");
+const RUTA = path.join(RUTACARPETA, NOMFITXER);
+console.log(RUTA);
+
+let llegir = fs.readFile(RUTA, 'utf8', (err,data) => { //utf8 passar d'hexadecimal a string
+  if (err) throw console.log(err);
+  console.log(data);
+});
