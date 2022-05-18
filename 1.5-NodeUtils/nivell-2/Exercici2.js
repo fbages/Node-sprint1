683756e28087fa80acc0e0a8eecbf9835218bfb1
deltakosmos <francescbages@gmail.com>
2022-05-18 14:46:57 +0200

    1.5 - nivell 2 - Exercici2

---
+++ 1.5-NodeUtils/nivell-2/Exercici2.js
@@ -0,0 +1,10 @@
+//Modul pels executar subprocessos
+const { exec } = require('child_process');
+
+//Si el proces requereix de molts dades millor utilitzar spawn, ja que exec guardar en el buffer i ocupar memoria
+//Commands van lligats amb &
+exec('cd/ & dir', (err, stdout, stderr) => {
+    if (err) console.log(err.message)
+    if (stderr) console.log(stderr)
+    console.log(stdout);
+})
