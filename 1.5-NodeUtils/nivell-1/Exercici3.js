6063fdc12999b48ff90e1697b16f4335ca5835c9
unknown <francescbages@gmail.com>
2022-05-18 10:50:13 +0200

    1.5 - nivell 1 - Exercici3

---
+++ 1.5-NodeUtils/nivell-1/Exercici3.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require("path");
+const { pipeline } = require('stream');
+const zlib = require('zlib');
+
+//Constants
+const NOMFITXER = "text.txt";
+const RUTACARPETA = path.join(__dirname, "fitxers");
+const RUTAFITXER = path.join(RUTACARPETA, NOMFITXER);
+const RUTADESTI = path.join(RUTACARPETA,'comprimit.txt.gz');
+
+
+//Readstream
+const source = fs.createReadStream(RUTAFITXER);
+const desti = fs.createWriteStream(RUTADESTI);
+
+//Pipeline
+
+pipeline(source, zlib.createGzip(),desti,(err)=>{
+    if (err) throw console.log(err);
+    console.log(`Fitxer comprimit en la direcció ${RUTADESTI}`)
+});
+
+//o per Pipe
+//source.pipe(zlib.createGzip()).pipe(desti);
